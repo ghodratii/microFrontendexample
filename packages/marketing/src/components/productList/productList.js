5248d25904074a384f86dcd34df564f15df3cc6f
Rana Ghodrati <R.Ghodrati@TNC.COM>
2021-10-04 12:07:05 +0330

    fixed compareList bug

--- packages/marketing/src/components/productList/productList.js
+++ packages/marketing/src/components/productList/productList.js
@@ -6,13 +6,13 @@ import "./productList.style.scss";
 
 class ProductList extends React.Component {
   componentDidMount() {
-    const { fetchProducts,selectItems } = this.props;
-    if(selectItems.length===0)
+    const { fetchProducts, selectItems } = this.props;
+    if (selectItems.length === 0)
 
-    fetchProducts();
+      fetchProducts();
   }
   // checkProduct = () => {
-    
+
   //   const { productList,selectItems } = this.props;
   //   productList.map((item)=>{
   //     if(selectItems.includes(item))
@@ -24,21 +24,29 @@ class ProductList extends React.Component {
   // }
 
   render() {
-    const { productList,selectItems } = this.props;
-    const showProduct= productList?.filter((item,key)=>{
-      if (key<12) return item;
-    })
+    const { productList, selectItems } = this.props;
+    // const showProduct = productList?.filter((item, key) => {
+    //   if (key < 12) return item;
+    // })
 
     return (
       <div id="home-articles" className="py-2">
         <div className="container">
           <div className="articles-container">
-            {productList&&
-               productList.map((item) => 
-               selectItems.includes(item)?
-              <Product key={item.id} item={item} compare={false}/>
-              :<Product key={item.id} item={item} compare={true}/>)
-              }
+            {productList &&
+              productList.map((item) => {
+                const checkPro = selectItems.some(
+                  value => { return value.id == item.id });
+                if (checkPro)
+                  return <Product key={item.id} item={item} compare={false} />
+                else return <Product key={item.id} item={item} compare={true} />
+
+              })
+
+              //  selectItems.includes(item)?
+              // <Product key={item.id} item={item} compare={false}/>
+              // :<Product key={item.id} item={item} compare={true}/>)
+            }
           </div>
         </div>
       </div>
@@ -47,12 +55,12 @@ class ProductList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  const { producList,selectedProducts } = state;
+  const { producList, selectedProducts } = state;
 
   const productList = producList.productList;
   const selectItems = selectedProducts.selectItems;
 
-  return { productList,selectItems };
+  return { productList, selectItems };
 };
 
 
